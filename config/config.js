fadf8e638b1287924630f31731c5b8782330b7ef
Ricardo <ricardoarrobo@gmail.com>
2019-06-18 13:21:23 -0500

    cambios en la config

--- config/config.js
+++ config/config.js
@@ -1,7 +1,6 @@
 const url = require('url');
 // Puerto local
-process.env.PORT = 3000;
+process.env.PORT = process.env.PORT || 3000;
 
 // URL instancia de MongoDB
-
 process.env.MONGOURI = 'mongodb+srv://ricardo_arr:krarrobo_98@cafe-zqczv.mongodb.net/test?retryWrites=true&w=majority';
